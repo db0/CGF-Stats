0e2ca50960c6f5bcb71c7ee852e8675c649099cc
Divided by Zer0 <mail@dbzer0.com>
2021-03-06 22:41:37 +0100

    still cors

--- cgf-stats.py
+++ cgf-stats.py
@@ -32,6 +32,8 @@ def write_to_disk():
 @REST_API.after_request
 def after_request(response):
 	response.headers["Access-Control-Allow-Origin"] = "*"
+	response.headers["Access-Control-Allow-Credentials"] = "true"
+	response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
 	response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
 	return response
 
