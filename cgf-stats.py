222f9079248cbac6c0fe1d148fc839ccbacd1342
Divided by Zer0 <mail@dbzer0.com>
2021-03-09 18:09:20 +0100

    added game pruner to delete unfinished games

--- cgf-stats.py
+++ cgf-stats.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from datetime import datetime
 import json, os
 import argparse
-
+import threading, time
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument(dest='gamename', help="The Name of the Godot Game")
@@ -18,9 +18,9 @@ arg_parser.add_argument('--states', action="store", default='defeat,victory', he
 REST_API = Flask(__name__)
 # Very basic DOS prevention
 limiter = Limiter(
-    REST_API,
-    key_func=get_remote_address,
-    default_limits=["90 per minute"]
+	REST_API,
+	key_func=get_remote_address,
+	default_limits=["90 per minute"]
 )
 api = Api(REST_API)
 
@@ -31,12 +31,39 @@ def write_to_disk():
 	with open("games", 'w') as db:
 		json.dump(games,db)
 
+
+class GamePruner(object):
+	# Every hour we delete all game which have been recorded unfinished for 5 hours or more.
+	def __init__(self, interval = 3600, seconds_to_wait = 18000):
+		self.interval = interval
+		self.seconds_to_wait = seconds_to_wait
+
+		thread = threading.Thread(target=self.prune_uncompleted_games, args=())
+		thread.daemon = True
+		thread.start()
+
+	def prune_uncompleted_games(self):
+		while True:
+			pruned_count = 0
+			for game in games.copy():
+				if games[game]['state'] == "unfinished":
+					start_time = datetime.strptime(games[game].get("start_datetime", ''), "%Y-%m-%d %H:%M:%S")
+					if (datetime.now() - start_time).seconds >= self.seconds_to_wait:
+						del games[game]
+						pruned_count += 1
+			if pruned_count >= 1:
+				print(f" * Pruned {pruned_count} uncompleted games from the database", flush=True)
+				write_to_disk()
+			time.sleep(self.interval)
+
+
 @REST_API.after_request
 def after_request(response):
 	response.headers["Access-Control-Allow-Origin"] = "*"
 	response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
 	response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
 	return response
+
 
 class NewGame(Resource):
 	decorators = [limiter.limit("4/minute")]
@@ -60,6 +87,7 @@ class NewGame(Resource):
 
 	def options(self):
 		return("OK", 200)
+
 
 class Game(Resource):
 	def get(self, gameid):
@@ -90,6 +118,7 @@ class Game(Resource):
 	def options(self, gameid):
 		return("OK", 200)
 
+
 if os.path.isfile("games"):
 	with open("games") as db:
 		games = json.load(db)
@@ -101,4 +130,5 @@ end_game_states = stat_args.states.split(',')
 api.add_resource(Game, "/game/<string:gameid>")
 api.add_resource(NewGame, "/newgame/")
 
+GamePruner()
 REST_API.run(debug=True,host=stat_args.ip,port=stat_args.port)
